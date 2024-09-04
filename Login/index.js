6c35409f5bb458e1328a583796e3a70c15106f46
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-09-04 17:12:11 +0530

    Update index.js

--- Login/index.js
+++ Login/index.js
@@ -26,8 +26,8 @@ class Login extends Component {
     event.preventDefault()
     let {username, password} = this.state
 
-    if (username.toLowerCase().trim(' ') === 'santosh') username = 'rahul'
-    if (password === 'santosh@2023') password = 'rahul@2021'
+    if (username.toLowerCase().trim(' ') === 'harikrishna') username = 'rahul'
+    if (password === 'hari@2023') password = 'rahul@2021'
 
     const userDetails = {username, password}
     const LoginApiUrl = 'https://apis.ccbp.in/login'
